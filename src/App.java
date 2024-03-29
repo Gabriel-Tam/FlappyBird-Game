806dd929e60aad8d22a0fa8aa2674c42952e86e7
Gabriel-Tam <gabrieltamayo.02.27@gmail.com>
2024-03-29 17:58:16 -0600

    Chance in floders

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
-
-
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
         int boardHeight = 640;
-
 
         JFrame frame = new JFrame("Flappy Bird");
         // frame.setVisible(true);
