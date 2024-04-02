18be52b5a647dd8b29f93303c6fa76b5909130e1
Gabriel-Tam <gabrieltamayo.02.27@gmail.com>
2024-04-02 13:40:43 -0600

    coments in the method world

--- src/World.java
+++ src/World.java
@@ -1,6 +1,6 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
+//import java.awt.Color;
+//import java.awt.Font;
+//import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
